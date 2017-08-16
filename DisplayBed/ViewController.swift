048cd66a77257869da650737ebfd16e73235990f
Luca Attanasio <luca@lucas-mbp.lan>
2017-08-16 19:45:36 +0200

    Initial Commit

---
+++ DisplayBed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DisplayBed
+//
+//  Created by Luca Attanasio on 16/08/2017.
+//  Copyright © 2017 Luca Attanasio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
